207c37c9964e08c5557d5872b4a4455cb7d98acf
Yuoa <jio@yuoa.dev>
2019-03-23 03:02:02 +0900

    With_wldh v1

---
+++ burri.js
@@ -0,0 +1,38 @@
+(function () {
+  var cq = document.cookie.split(';')
+  cq.forEach(function (ci) {
+    var c = ci.split('=')
+    if ('ptDark' === c[0].trim()) {
+      if (c[1][0] === 't') {
+        document.documentElement.style.setProperty('background-color', 'rgb(34, 34, 34)')
+        document.body.classList.add('dark')
+      } else {
+        document.documentElement.style.setProperty('background-color', 'rgb(234, 234, 234)')
+      }
+    }
+  })
+})
+
+window.onload = function () {
+  const setCookie = function (value) {
+    var date = new Date()
+    date.setTime(date.getTime() + 315360000000)
+    var expires = "expires=" + date.toUTCString()
+    document.cookie = "ptDark=" + value + ";" + expires + ";path=/;domain=widh.me"
+  }
+  const lightSwitch = document.getElementById('switch')
+  lightSwitch.onclick = function () {
+    var isDark = document.body.classList.contains('dark')
+    if (isDark) {
+      document.body.classList.remove('dark')
+      document.documentElement.style.setProperty('background-color', 'rgb(234, 234, 234)')
+    } else {
+      document.body.classList.add('dark')
+      document.documentElement.style.setProperty('background-color', 'rgb(34, 34, 34)')
+    }
+    setCookie(!isDark)
+  }
+  lightSwitch.onmouseup = function (event) {
+    event.target.blur()
+  }
+}
