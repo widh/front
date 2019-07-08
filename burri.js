faa53b85b83db89a90926087a07c87beede0505a
Jio <jio@widh.me>
2019-07-09 05:20:09 +0900

    Widh_wldh v1.1

--- burri.js
+++ burri.js
@@ -2,10 +2,11 @@
   var cq = document.cookie.split(';')
   cq.forEach(function (ci) {
     var c = ci.split('=')
-    if ('ptDark' === c[0].trim()) {
+    if ('modarkbul' === c[0].trim()) {
       if (c[1][0] === 't') {
         document.documentElement.style.setProperty('background-color', 'rgb(34, 34, 34)')
         document.documentElement.classList.add('dark')
+        document.querySelector('meta[name=theme-color]').content = '#343434'
       } else {
         document.documentElement.style.setProperty('background-color', 'rgb(234, 234, 234)')
       }
@@ -19,16 +20,18 @@ window.onload = function () {
     var date = new Date()
     date.setTime(date.getTime() + 315360000000)
     var expires = 'expires=' + date.toUTCString()
-    document.cookie = 'ptDark=' + value + ';' + expires + ';path=/;domain=widh.me'
+    document.cookie = 'modarkbul=' + value + ';' + expires + ';path=/;domain=widh.me'
   }
   const toggleLight = function () {
     var isDark = document.documentElement.classList.contains('dark')
     if (isDark) {
       document.documentElement.classList.remove('dark')
       document.documentElement.style.setProperty('background-color', 'rgb(234, 234, 234)')
+      document.querySelector('meta[name=theme-color]').content = '#eeeeee'
     } else {
       document.documentElement.classList.add('dark')
       document.documentElement.style.setProperty('background-color', 'rgb(34, 34, 34)')
+      document.querySelector('meta[name=theme-color]').content = '#343434'
     }
     setCookie(!isDark)
   }
@@ -45,4 +48,26 @@ window.onload = function () {
   window.addEventListener('beforeunload', function () {
     lightSwitch.style.setProperty('animation', 'opa-wave 2s infinite')
   })
+  // Changing banner
+  let textIter = 0
+  const textBanners = document.getElementById('banner').children
+  const textChanger = () => {
+    const nowText = textBanners[(textIter++) % textBanners.length]
+    const nextText = textBanners[textIter % textBanners.length]
+    nowText.classList.add('transparent')
+    window.setTimeout(function () {
+      nowText.classList.add('removed')
+      nextText.classList.remove('removed')
+      window.setTimeout(function () {
+        nextText.classList.remove('transparent')
+      }, 20)
+    }, 240)
+  }
+  [].slice.call(textBanners).slice(1).forEach(function (banner) {
+    banner.classList.add('transparent')
+    banner.classList.add('removed')
+  })
+  if (textBanners.length > 0) {
+    window.setInterval(textChanger, 5000)
+  }
 }
