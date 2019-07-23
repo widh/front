875e93de4a6d3efefeadc9e30ccf2456eeb14641
Jio <jio@widh.me>
2019-07-23 11:29:11 +0900

    With_wldh v1.3.8 - Minor feature upgrade

--- burri.js
+++ burri.js
@@ -15,17 +15,15 @@
 
 window.onload = function () {
   // Lightmode
-  if (document.documentElement.classList.contains('dark')) {
+  if (document.documentElement.classList.contains('dark'))
     document.querySelector('meta[name=theme-color]').content = '#000000';
-  }
   const setCookie = function (value) {
-    var date = new Date();
+    const date = new Date();
     date.setTime(date.getTime() + 315360000000);
-    var expires = 'expires=' + date.toUTCString();
-    document.cookie = 'modarkbul=' + value + ';' + expires + ';path=/;domain=widh.me';
+    document.cookie = `modarkbul=${value};expires=${date.toUTCString()};path=/;domain=widh.me`;
   };
-  const toggleLight = function () {
-    var isDark = document.documentElement.classList.contains('dark');
+  const toggleLight = () => {
+    const isDark = document.documentElement.classList.contains('dark');
     if (isDark) {
       document.documentElement.classList.remove('dark');
       document.documentElement.style.setProperty('background-color', 'rgb(240, 240, 240)');
@@ -39,35 +37,29 @@ window.onload = function () {
   };
   const lightSwitch = document.getElementById('switch');
   lightSwitch.onclick = toggleLight;
-  lightSwitch.onmouseup = function (event) {
-    event.target.blur();
-  };
-  lightSwitch.ontouchend = function (event) {
-    event.target.blur();
-  };
 
   // Loader
   lightSwitch.style.setProperty('animation', 'none');
-  window.addEventListener('beforeunload', function () {
-    lightSwitch.style.setProperty('animation', 'opa-wave 2s infinite');
+  window.addEventListener('beforeunload', () => {
+    lightSwitch.style.setProperty('animation', 'shiney 3.2s ease-in-out infinite');
   });
 
   // Changing banner
   let textIter = 0;
   const textBanners = document.getElementById('banner').querySelectorAll('span');
-  const textChanger = function () {
+  const textChanger = () => {
     const nowText = textBanners[(textIter++) % textBanners.length];
     const nextText = textBanners[textIter % textBanners.length];
     nowText.classList.add('transparent');
-    window.setTimeout(function () {
+    window.setTimeout(() => {
       nowText.classList.add('removed');
       nextText.classList.remove('removed');
-      window.setTimeout(function () {
+      window.setTimeout(() => {
         nextText.classList.remove('transparent');
       }, 20);
     }, 240);
   };
-  Array.prototype.slice.call(textBanners).slice(1).forEach(function (banner) {
+  Array.prototype.slice.call(textBanners).slice(1).forEach((banner) => {
     banner.classList.add('transparent');
     banner.classList.add('removed');
   });
@@ -75,13 +67,12 @@ window.onload = function () {
     window.setInterval(textChanger, 5000);
   }
 
-  // Onclick blurer
-  const links = document.querySelectorAll('#links a');
-  links.forEach(function (link) {
-    link.onclick = function () {
-      if (document.activeElement) {
-        document.activeElement.blur();
-      }
-    };
-  });
+  // Active blurer
+  const blurer = () => {
+    if (document.activeElement) {
+      document.activeElement.blur();
+    }
+  };
+  document.body.onmouseup = blurer;
+  document.body.ontouchend = blurer;
 };
